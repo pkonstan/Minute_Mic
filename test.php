332d189791dc07e6dfa26568d70819a7fcf5e60b
Paul Konstanski <paul.konstanski@cru.org>
2018-09-06 17:44:24 -0400

    Create test.php
    This is just a test to see if I can put a proposed file into your branch.

---
+++ test.php
@@ -0,0 +1,17 @@
+<?
+$ttl = $_SERVER['SERVER_NAME'];
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title><?=ucfirst($ttl)." - Homepage"?></title>
+</head>
+
+<body>
+<h1>You are at <?=ucfirst($ttl)?></h1>
+<p>We are in the process of moving servers... please stand by</p>
+<p>If you need immediate assistance, please <a href="http://cruglobal.freshdesk.com/support/tickets/new">Submit a Support Ticket</a>.</p>
+<img src="servermove.jpg" alt="View from Lionshead, Cape Town South Africa" />
+</body>
+</html>
